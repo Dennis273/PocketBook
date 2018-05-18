f25093530a7d986fc29c29c55d187398d90dc162
Dennis273 <dennisc695@gmail.com>
2018-05-19 00:01:56 +0800

    Rename some classes

--- PocketBook/DetailMonthView.xaml.cs
+++ PocketBook/DetailMonthView.xaml.cs
@@ -20,9 +20,9 @@ namespace PocketBook
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class DetailView : Page
+    public sealed partial class DetailMonthView : Page
     {
-        public DetailView()
+        public DetailMonthView()
         {
             this.InitializeComponent();
         }
