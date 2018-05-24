ddb6d2e9866524b615b09dd5dcdc7d181dc802a5
Dennis273 <dennisc695@gmail.com>
2018-05-24 14:21:00 +0800

    Add DataProvider

--- PocketBook/DataStructure.cs
+++ PocketBook/DataStructure.cs
@@ -11,32 +11,13 @@ namespace PocketBook
         public float Money { get; set; }
         public DateTime SpendDate { get; set; }
         public string Catagory { get; set; }
+        public string Comment { get; set; }
         public DataEntry(float money, DateTime spendDate, string catagory)
         {
             Money = money;
             SpendDate = spendDate;
             Catagory = catagory;
         }
-    }
-
-    public class CatagoryHandler
-    {
-        public List<string> Catagories;
-        private CatagoryHandler()
-        {
-            Catagories = new List<string>();
-        }
-
-        // Check if s exist in Catagories
-        static public CatagoryHandler GetCatagoryHandler()
-        {
-            if (m == null)
-            {
-                m = new CatagoryHandler();
-            }
-            return m;
-        }
-        static private CatagoryHandler m;
     }
     
     public class DayData
